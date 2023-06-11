7068a6d726af131c8fd309b084e226ca721628f3
GT17S <guenanetoufik0615@gmail.com>
2023-06-11 19:57:24 +0200

    add loggers for the reader,writer and processor.

--- src/main/java/com/example/demo/processor/StudentItemProcessor.java
+++ src/main/java/com/example/demo/processor/StudentItemProcessor.java
@@ -2,14 +2,18 @@ package com.example.demo.processor;
 
 import com.example.demo.db.Student;
 import com.example.demo.dto.StudentDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 
 public class StudentItemProcessor implements ItemProcessor<Student, StudentDto> {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(StudentItemProcessor.class);
 
     @Override
     public StudentDto process(Student item) throws Exception {
 
+        LOGGER.info("Processing Students to DTOs: ");
         StudentDto studentDto = new StudentDto();
         studentDto.setNom(item.getNom());
         studentDto.setAge(item.getAge());
--- src/main/java/com/example/demo/writer/StudentItemWriter.java
+++ src/main/java/com/example/demo/writer/StudentItemWriter.java
@@ -1,6 +1,8 @@
 package com.example.demo.writer;
 
 import com.example.demo.dto.StudentDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
@@ -9,14 +11,13 @@ import org.springframework.core.io.FileSystemResource;
 
 public class StudentItemWriter extends FlatFileItemWriter<StudentDto> {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(StudentItemWriter.class);
 
     public StudentItemWriter() {
 
-
+        LOGGER.info("Writing in the output file: ");
         this.setResource(new FileSystemResource("output.csv"));
-
         this.setAppendAllowed(true);
-
         this.setLineAggregator(new DelimitedLineAggregator<StudentDto>() {
             {
                 setDelimiter(",");
